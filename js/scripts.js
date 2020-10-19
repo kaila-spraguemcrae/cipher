ec95c6352c6c73a79aa22efa834d9d2554cf8a47
Kaila Sprague McRae <kaila.sprague@icloud.com>
2020-10-19 14:42:29 -0700

    add imgs and functions
    co-authored-by: Ben McFarland <benrmcfarland@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,23 @@ function lettersToUpper(string) {
 
   return firstLetter.toUpperCase() + lastLetter.toUpperCase();
 }
+
 function switchLetters(string) {
   return string.charAt(1) + string.charAt(0);
+}
+
+function newSentence(string, fullSentence) {
+  return fullSentence + string;
+}
+
+function middleLetter(string, sentence) {
+  const halfLetter = (sentence.length) / 2;
+
+  return sentence.charAt(halfLetter) + string;
+}
+
+function reverseString(str) {
+  return str.split("").reverse().join("");
 }
 
 
@@ -13,5 +28,26 @@ const sentence = prompt("What is your sentence?");
 
 //alert(sentence);
 
+const upperLetters = lettersToUpper(sentence);
+//alert(upperLetters);
 
-alert(switchLetters(lettersToUpper(sentence)));
+const switchedLetters = switchLetters(upperLetters);
+//alert(switchedLetters);
+
+const bigSentence = newSentence(switchedLetters, sentence);
+//alert(bigSentence);
+
+const biggerSentence = middleLetter(bigSentence, sentence);
+//alert(biggerSentence);
+
+const reversed = reverseString(biggerSentence);
+//alert(reversed);
+
+$(document).ready(function() {
+  $(".original").click(function() {
+    $(".sentence").html(sentence);
+  });
+  $(".new").click(function() {
+    $(".sentence").html(reversed);
+  });
+});
